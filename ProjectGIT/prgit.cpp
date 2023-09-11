a0b89643d3a33a351858f40dfb816a069f5ef2e6
Demian Rymar <ua.demyan@gmail.com>
2023-09-11 21:25:25 +0300

    second branch

--- ProjectGIT/prgit.cpp
+++ ProjectGIT/prgit.cpp
@@ -9,11 +9,10 @@ int main() {
 	cin >> a;
 
 	double z1 = (sin((M_PI / 2.0) + (3.0 * a))) / ((1.0 - (sin((3.0 * a) - M_PI))));
-	double z2 = (1.0 / ((tan(((5.0 / 4.0) * M_PI) + ((3.0 / 2.0) * a)))));
+	//double z2 = (1.0 / ((tan(((5.0 / 4.0) * M_PI) + ((3.0 / 2.0) * a)))));
 
-	//test
-	//double exp = (5.0 / 4.0) * M_PI + (3.0 / 2.0) * a;
-	//double z2 = 1.0 / tan(exp);
+	double exp = (5.0 / 4.0) * M_PI + (3.0 / 2.0) * a;
+	double z2 = 1.0 / tan(exp);
 
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
